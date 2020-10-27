0b89ac7312081aeed9d6348c4b7cbd820a237cd4
N-Harish <48133371+N-Harish@users.noreply.github.com>
2020-10-27 18:04:49 +0530

    Update demotensor.py

--- demotensor.py
+++ demotensor.py
@@ -10,7 +10,7 @@ test_images = test_images / 255.0
 
 if st.button("predict"):
     model = tf.keras.models.load_model("model.h5")
-    test_img = test_images[0]
+    test_img = test_images
     pred = model.predict(test_img)
 
     st.success(pred)
