05080ccdcf083845b51a9029997c352cc48f51d2
N-Harish <48133371+N-Harish@users.noreply.github.com>
2020-10-27 18:03:43 +0530

    Update demotensor.py

--- demotensor.py
+++ demotensor.py
@@ -6,9 +6,11 @@ fashion_mnist = tf.keras.datasets.fashion_mnist
 
 (train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+test_images = test_images / 255.0
+
 if st.button("predict"):
     model = tf.keras.models.load_model("model.h5")
-    test_img = test_images[0] / 255.0
+    test_img = test_images[0]
     pred = model.predict(test_img)
 
     st.success(pred)
